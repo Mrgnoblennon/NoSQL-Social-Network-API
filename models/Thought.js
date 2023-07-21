19efae3e10bd2cbd24daf58537215b873e0d1c21
Frank Ocean <chasebarrettbrown@hotmail.com>
2023-07-21 11:12:34 +0800

    added models

---
+++ models/Thought.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toISOString()
+  }
+});
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toISOString()
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema]
+});
+
+// Create a virtual property `reactionCount` that retrieves the length of the `reactions` array
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
