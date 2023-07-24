15901106daf52b26d44841d7e17ec6f1ebf7f22f
Frank Ocean <chasebarrettbrown@hotmail.com>
2023-07-25 02:15:40 +0800

    established connection to mongoose

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,22 @@
+// Import required modules from mongoose
 const { connect, connection } = require('mongoose');
 
+// Define the connection string to the MongoDB database
+// If the environment variable MONGODB_URI is set, use its value; otherwise, use the local connection string
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetwork';
 
+// Connect to the MongoDB database using Mongoose with error handling
 connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  useNewUrlParser: true,         // Use the new URL parser
+  useUnifiedTopology: true,    // Use the new unified topology engine
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
+// Export the connection object to be used in other parts of the application
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const db = require('./config/connection');
 //const routes = require('./routes');
+const User = require('./models/User'); // Import the User model
+const Thought = require('./models/Thought'); // Import the Thought model
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`Check it out on port ${PORT}!`);
+    console.log(`Check it out ${PORT}!`);
   });
 });
